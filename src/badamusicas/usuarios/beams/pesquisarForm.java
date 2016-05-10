1be33a99e197b2b90a7a2b1b4f05aa0ec864850b
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-05-09 22:11:34 -0300

    pesquisar musicas

---
+++ src/badamusicas/usuarios/beams/pesquisarForm.java
@@ -0,0 +1,45 @@
+package badamusicas.usuarios.beams;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import badamusicas.usuarios.Lista;
+import badamusicas.usuarios.Usuario;
+
+@ManagedBean(name="pesquisarForm")
+@RequestScoped
+
+public class pesquisarForm {
+//	private ArrayList<Musica> musicas;
+//	private ArrayList<Album> albuns;
+	private Lista selectedList;
+	private Usuario user;
+	
+	public pesquisarForm(){
+		setSelectedList(new Lista());
+		user= getSessionUser();
+	}
+	
+	private Usuario getSessionUser(){
+		Usuario u = new Usuario();
+		
+		u = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuarioLogado");
+		
+		return u;
+	}
+	
+	public String adicionar(){
+		
+		
+		return null;
+	}
+
+	public Lista getSelectedList() {
+		return selectedList;
+	}
+
+	public void setSelectedList(Lista selectedList) {
+		this.selectedList = selectedList;
+	}
+}
