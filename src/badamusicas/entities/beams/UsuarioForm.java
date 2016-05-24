a8313ca9c9ca7a6229162f07170873f29f6389a7
Bárbara Perina <barbara.perinabezerra@gmail.com>
2016-05-23 21:54:18 -0300

    commit final

--- src/badamusicas/entities/beams/UsuarioForm.java
+++ src/badamusicas/entities/beams/UsuarioForm.java
@@ -4,76 +4,91 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 import badamusicas.DAO.UsuarioDao;
 import badamusicas.entities.Usuario;
 
-@ManagedBean(name="usuarioForm")
+@ManagedBean(name = "usuarioForm")
 @RequestScoped
 
 public class UsuarioForm {
-	
+
 	private Usuario usuario;
 	private boolean cadastro;
-	
-	public UsuarioForm(){
-		usuario  = new Usuario();
+	UsuarioDao udao = new UsuarioDao();
+
+	public UsuarioForm() {
+		usuario = new Usuario();
 	}
-	
-	public String fazerLogin(){
+
+	public String fazerLogin() {
 		UsuarioDao udao = new UsuarioDao();
 		Usuario u = udao.buscar(usuario.getLogin());
-		if (u != null && u.validarSenha(usuario.getSenha())){
-			//FacesContext.getCurrentInstance().getExternalContext().getSessionMap()  - pegar a session
+		if (u != null && u.validarSenha(usuario.getSenha())) {
+			// FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+			// - pegar a session
 			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuarioLogado", u);
 			return "gerenciarlistas";
-		}else{
-			FacesContext.getCurrentInstance()
-				.addMessage(null, new FacesMessage("Usuário não encontrado ou senha incorreta."));
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("Usuário não encontrado ou senha incorreta."));
 		}
 		return null;
 	}
-	
-	public String cadastrarUsuario(Usuario usuario){
-		UsuarioDao udao = new UsuarioDao();
+
+	public String fazerLogoff() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+		session.invalidate();
+		return "index";
+	}
+
+	public String cadastrarUsuario(Usuario usuario) {
 		udao.adicionar(usuario);
 		return "index";
 	}
-	
-	public String cadastrar(){
+
+	public String cadastrar() {
 		setCadastro(true);
 		return "cadastro";
 	}
-	
-	public String voltar(){
-		if(getCadastro()){
+
+	public String alterarUsuario(Usuario usuario) {
+		udao.alterar(usuario);
+		return fazerLogoff();
+	}
+
+	public String voltar() {
+		if (getCadastro()) {
 			System.out.println("index");
 			return "index";
 		}
 		System.out.println("gerenciar listas");
 		return "gerenciarlistas";
 	}
-	
-	public String checkloged(){
-		if(getSessionUser() == null){
+
+	public String checkloged() {
+		if (getSessionUser() == null) {
 			return null;
-		}else{
+		} else {
 			return "gerenciarlistas";
 		}
 	}
-	
-	private Usuario getSessionUser(){
+
+	private Usuario getSessionUser() {
 		Usuario u = new Usuario();
-		
+
 		u = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuarioLogado");
-		
+
 		return u;
 	}
-	
-	public Usuario getUsuario(){
+
+	public Usuario getUsuario() {
 		return this.usuario;
 	}
-	public void setUsuario(Usuario usuario){
+
+	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
 
